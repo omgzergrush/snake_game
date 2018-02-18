573f355bf57ffbb873a018add7f9edb078af3445
Pekka Alho <pekka.alho@gmail.com>
2018-02-18 22:03:35 +0200

    Snake head graphics

--- game1.py
+++ game1.py
@@ -23,6 +23,7 @@ pygame.display.set_caption("Snake")
 # global objects
 font = pygame.font.SysFont(None, 25)
 clock = pygame.time.Clock()
+img_head = pygame.image.load('snake_head.png')
 
 
 def message_to_screen(message, color):
@@ -32,35 +33,41 @@ def message_to_screen(message, color):
     game_display.blit(text_surface, text_rect)
 
 
-def event_handler(game_exit, block_size, pos_change):
+def event_handler(game_exit, block_size, pos_change, direction):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_exit = True
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT and event_handler.previous_event != pygame.K_RIGHT:
+            if event.key == pygame.K_LEFT and "direction" != "right":
                 pos_change[0] = -speed * block_size
                 pos_change[1] = 0
-                event_handler.previous_event = pygame.K_LEFT
-            if event.key == pygame.K_RIGHT and event_handler.previous_event != pygame.K_LEFT:
+                direction = "left"
+            if event.key == pygame.K_RIGHT and "direction" != "left":
                 pos_change[0] = speed * block_size
                 pos_change[1] = 0
-                event_handler.previous_event = pygame.K_RIGHT
-            if event.key == pygame.K_UP and event_handler.previous_event != pygame.K_DOWN:
+                direction = "right"
+            if event.key == pygame.K_UP and "direction" != "down":
                 pos_change[0] = 0
                 pos_change[1] = -speed * block_size
-                event_handler.previous_event = pygame.K_UP
-            if event.key == pygame.K_DOWN and event_handler.previous_event != pygame.K_UP:
+                direction = "up"
+            if event.key == pygame.K_DOWN and "direction" != "up":
                 pos_change[0] = 0
                 pos_change[1] = speed * block_size
-                event_handler.previous_event = pygame.K_DOWN
-    return game_exit
+                direction = "down"
+    return game_exit, direction
 
 
-event_handler.previous_event = 0
-
-
-def snake_draw(snake_body, block_size):
-    for snake_piece in snake_body:
+def snake_draw(snake_body, block_size, direction):
+    if direction == "right":
+        head = img_head
+    elif direction == "left":
+        head = pygame.transform.rotate(img_head, 180)
+    elif direction == "up":
+        head = pygame.transform.rotate(img_head, 90)
+    elif direction == "down":
+        head = pygame.transform.rotate(img_head, 270)
+    game_display.blit(head, [snake_body[-1][0], snake_body[-1][1]])
+    for snake_piece in snake_body[:-1]:
         game_display.fill(green, [snake_piece[0], snake_piece[1], block_size, block_size])
 
 
@@ -73,7 +80,8 @@ def random_pos(object_size):
 def game_loop():
     game_exit = False
     game_over = False
-    pos_change = [0, 0]
+    pos_change = [10, 0]
+    direction = "right"
     start_position = [board_width / 2, board_height / 2]  # x, y
     snake_head = start_position
     block_size = 20
@@ -98,7 +106,7 @@ def game_loop():
                     if event.key == pygame.K_c:
                         game_loop()
 
-        game_exit = event_handler(game_exit, block_size, pos_change)
+        game_exit, direction = event_handler(game_exit, block_size, pos_change, direction)
 
         # MAIN LOGIC
         snake_head[0] += pos_change[0]
@@ -128,7 +136,7 @@ def game_loop():
         if not game_over:
             game_display.fill(white)  # clean the game board
             game_display.fill(red, [apple_pos[0], apple_pos[1], apple_size, apple_size])
-            snake_draw(snake_body, block_size)
+            snake_draw(snake_body, block_size, direction)
             pygame.display.update()  # frame done, render it
 
         clock.tick(FPS)  # don't change fps for difficulty, change movement speed
